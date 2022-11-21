2d165b3f4f7fefabc36edcadd2cbdd55936983fa
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 <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>
2022-11-21 20:33:17 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+import Search from './screens/Search';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: 'red',
+      }}>
+        <Tab.Screen 
+          name='Home' 
+          component={HomeScreen}
+          options={{
+            tabBarIcon:()=> 
+            <Entypo name='home' size={22} color='blue' />
+          }}
+        />
+        <Tab.Screen 
+          name='Search' 
+          component={Search} 
+          options={{
+            tabBarIcon:()=> 
+            <FontAwesome name="search" size={22} color="blue" />,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
